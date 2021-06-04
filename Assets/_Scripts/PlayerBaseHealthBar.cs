25feab81251c7670e40362b198237e1957afd04b
Luiz Vitor <luvi@germanos.ws>
2021-06-04 00:48:34 -0300

    hud

---
+++ Assets/_Scripts/PlayerBaseHealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBaseHealthBar : MonoBehaviour
+{
+    public GameManager gm;
+
+    // Health Bar
+    public int maxHealth = 5;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+        //maxHealth = gm.playerBaseHealth;
+        Debug.Log("BASE HEALTH:"+ maxHealth);
+        healthBar.SetMaxHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealth = gm.playerBaseHealth;
+        healthBar.SetHealth(currentHealth);
+        Debug.Log("BASE HEALTH UPDATE:" + currentHealth);
+
+    }
+}
